4e6e852227523a168a7a0fe252d552b7963278d4
EduardoCampos <edu.ggoncalves37@gmail.com>
2020-12-11 16:36:44 -0300

    some changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var user = {};
 function addLine(dataUser){
    var tr = document.createElement("tr");
    
-   tr.innerHTML = `
+   document.getElementById("table-users").innerHTML = `
       <tr>
          <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
          <td>${dataUser.name}</td>
@@ -18,7 +18,6 @@ function addLine(dataUser){
       </tr>
    `;
 
-   document.getElementById("table-users").appendChild(tr);
 }
 
 document.getElementById("form-user-create").addEventListener("submit", function(event){
@@ -40,6 +39,7 @@ document.getElementById("form-user-create").addEventListener("submit", function(
       
    });
 
-   addLine(user);
+   var object_user = new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
+   addLine(object_user);
    
 });
