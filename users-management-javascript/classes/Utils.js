90afe4e3a2a4e3f77a0cc793e98d612902440104
Eduardo <edu.ggoncalves37@gmail.com>
2021-07-19 18:20:52 -0300

    fix: :bug: fixing empty directory bug commited

---
+++ users-management-javascript/classes/Utils.js
@@ -0,0 +1,8 @@
+class Utils {
+
+    static dateFormat(date) {
+
+        return date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes();
+    }
+    
+}
