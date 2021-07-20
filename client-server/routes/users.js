c874ec5f9ee86b6d9643cd0897391aa81f78b415
Eduardo <edu.ggoncalves37@gmail.com>
2021-07-19 22:27:05 -0300

    feat: :sparkles: restify to access api rest

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var restify = require('restify-clients');
+var assert = require('assert');
 var router = express.Router();
+
+// Creates a JSON client
+var client = restify.createJsonClient({
+  url: 'http://localhost:4000'
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  
+  client.get('/users', function (err, request, response, obj) {
+    
+    assert.ifError(err);
+
+    res.end(JSON.stringify(obj, null, 2));
+  });
+
 });
 
 module.exports = router;
