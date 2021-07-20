4952d12cefe3446a5ac11d9292ae4f255d604ad4
Eduardo <edu.ggoncalves37@gmail.com>
2021-07-20 14:43:21 -0300

    refactor: :sparkles: adding routes( get/id, post, put, delete)

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -15,7 +15,51 @@ router.get('/', function(req, res, next) {
     
     assert.ifError(err);
 
-    res.end(JSON.stringify(obj, null, 2));
+    res.json(obj);
+  });
+
+});
+
+router.get('/:id', function(req, res, next) {
+  
+  client.get(`/users/${req.params.id}`, function (err, request, response, obj) {
+    
+    assert.ifError(err);
+
+    res.json(obj);
+  });
+
+});
+
+router.post('/', function(req, res, next) {
+  
+  client.post('/users', req.body,function (err, request, response, obj) {
+    
+    assert.ifError(err);
+
+    res.json(obj);
+  });
+
+});
+
+router.put('/:id', function(req, res, next) {
+  
+  client.put(`/users/${req.params.id}`, req.body,function (err, request, response, obj) {
+    
+    assert.ifError(err);
+
+    res.json(obj);
+  });
+
+});
+
+router.delete('/:id', function(req, res, next) {
+  
+  client.del(`/users/${req.params.id}`, function (err, request, response, obj) {
+    
+    assert.ifError(err);
+
+    res.json(obj);
   });
 
 });
