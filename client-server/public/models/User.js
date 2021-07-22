effe871455320709e85a5555684b25fbc9763e51
Eduardo <edu.ggoncalves37@gmail.com>
2021-07-22 00:05:41 -0300

    refactor: :recycle: refactoring http requests with fetch

--- client-server/public/models/User.js
+++ client-server/public/models/User.js
@@ -76,7 +76,7 @@ class User{
 
     static getUsersStorage(){
 
-        return HttpRequest.get('/users');
+        return Fetch.get('/users');
     }//getUsersStorage
 
     save(){
@@ -87,11 +87,11 @@ class User{
 
             if(this.id) {
 
-                promise = HttpRequest.put(`/users/${this.id}`, this.toJSON());
+                promise = Fetch.put(`/users/${this.id}`, this.toJSON());
             }
             else {
 
-                promise = HttpRequest.post('/users', this.toJSON());
+                promise = Fetch.post('/users', this.toJSON());
             }
 
             promise.then(data => {
@@ -112,7 +112,7 @@ class User{
 
     remove(){
         
-        return HttpRequest.delete(`/users/${this.id}`)
+        return Fetch.delete(`/users/${this.id}`)
     }//remove
 
     toJSON(){
